9b3c7d69a37c5425e474d03b61eb3bccdff169f2
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2017-05-11 22:49:10 +0200

    Add result filtering.

--- index.js
+++ index.js
@@ -2,64 +2,83 @@
 
 //////////////////////////// SEARCH BAR STYLE /////////////////////////
 
-  $('.searchbar--sticky').sticky({
-    topSpacing: 100,
-    getWidthFrom: '.searchbar--sticky',
-  })
+$('.searchbar--sticky').sticky({
+  topSpacing: 100,
+  getWidthFrom: '.searchbar--sticky',
+})
 
-  const searchBarOpacity = ko.observable(0)
+const scrollTop = ko.observable(0)
+const isTyping = ko.observable(false)
 
-  $(window).scroll(() => {
-    searchBarOpacity(Math.min($(window).scrollTop() / 200, 1))
-  })
+// Update observables as they change.
+$(window).scroll(() => { scrollTop($(window).scrollTop()) })
+$('.searchbar__input').blur(() => { isTyping($('.searchbar__input').is(':focus')) })
 
-//////////////////////////// GENERATE CONTENT /////////////////////////
+const searchBarOpacity = ko.pureComputed(() => {
+  const _scrollTop = scrollTop(),
+    _searchText = searchText(),
+    _isTyping = isTyping()
 
-  function generateMetasyntacticText () {
-    var words = 'eum et optio porro consequatur quia ullam nulla ipsa voluptatem ullam qui consectetur nobis corporis eius numquam sunt consequatur et iusto consequuntur consectetur quasi nisi blanditiis et minima eos qui quisquam iure quia excepturi dignissimos explicabo'.split(' ')
-
-    return 'Lorem ipsum ' + repeat(30, pickOne(words)).join(' ') + '.'
-  }
-
-  // [a] -> a
-  function pickOne (array) {
-    const index = Math.floor(Math.random() * array.length)
-    return array[index]
-  }
-
-  function repeat (n, a) {
-    return Array.apply(undefined, Array(n)).map(() => a)
-  }
+  return _searchText || _isTyping ? 1 : Math.min(_scrollTop / 200, 1)
+})
 
 //////////////////////////// HIGHLIGHT SEARCH /////////////////////////
 
-  const highlight = text => {
-    if (text.length < 2) return
+const highlight = text => {
+  if (text.length < 2) return
 
-    new HR('.metasyntactic', {
-      highlight: [searchText()],
-      backgroundColor: '#B4FFEB',
-    }).hr()
-  }
+  new HR('li.metasyntactic__item', {
+    highlight: [searchText()],
+    backgroundColor: '#B4FFEB',
+  }).hr()
+}
 
-  // Undoes de effects of HR. Details obtained from the library's source code.
-  const unhighlight = () => {
-    const metasyntactic = document.querySelector('.metasyntactic')
+// Undoes de effects of HR. Details obtained from the library's source code.
+const unhighlight = () => {
+  const metasyntactic = document.querySelectorAll('.metasyntactic__item')
 
-    metasyntactic.innerHTML = metasyntactic.innerHTML
+  Array.prototype.slice.call(metasyntactic).forEach(item => {
+    item.innerHTML = item.innerHTML
       .replace(new RegExp(/<span data-hr[^>]*>([^<]*)<\/span>/g), '$1')
-  }
+  })
+}
 
-  const searchText = ko.observable('')
-  searchText.subscribe(unhighlight)
-  searchText.subscribe(highlight)
+const searchText = ko.observable('')
+searchText.subscribe(unhighlight)
+searchText.subscribe(highlight)
+
+//////////////////////////// GENERATE CONTENT /////////////////////////
+
+function generateMetasyntacticText () {
+  var words = 'eum et optio porro consequatur quia ullam nulla ipsa voluptatem ullam qui consectetur nobis corporis eius numquam sunt consequatur et iusto consequuntur consectetur quasi nisi blanditiis et minima eos qui quisquam iure quia excepturi dignissimos explicabo'.split(' ')
+
+  return 'Lorem ipsum ' + repeat(30, pickOne(words)).join(' ') + '.'
+}
+
+// [a] -> a
+function pickOne (array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+function repeat (n, a) {
+  return Array.apply(undefined, Array(n)).map(() => a)
+}
+
+const items = repeat(25).map(() => ({ text: generateMetasyntacticText() }))
+const filteredItems = ko.pureComputed(function () {
+  const result = items
+    .filter(item => item.text.indexOf(searchText()) !== -1)
+  console.log(result)
+  return result
+})
 
 //////////////////////////// BIND TEMPLATE /////////////////////////
 
-  ko.applyBindings({
-    generateMetasyntacticText,
-    repeat,
-    searchText,
-    searchBarOpacity,
-  })
+ko.applyBindings({
+  filteredItems,
+  searchText,
+  searchBarOpacity,
+})
+
 }()
